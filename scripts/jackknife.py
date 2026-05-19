00feb0a5e5ed58e30806b37e91e660132c9186a7
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2026-05-18 17:26:02 -0700

    Plotting new results

--- scripts/jackknife.py
+++ scripts/jackknife.py
@@ -6,7 +6,8 @@ variants = [
         name=f"jackknife_{i}",
         exclude_jk=i,
         r_bins=np.geomspace(1e-2, 1, 5),
-        r_aper=3.0,
+        r_aper_min=2,
+        r_aper_max=4,
     )
     for i in range(0, 9)
 ]
